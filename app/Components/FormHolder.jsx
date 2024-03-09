f2938388a4fc779c8e0a78bc1611fcef7e7de511
Houssam Righi <hrighi01@gmail.com>
2024-03-09 13:57:14 -0500

    Refactor form layout and background image in FormHolder and HomeWrapper components

--- app/Components/FormHolder.jsx
+++ app/Components/FormHolder.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function FormHolder(props) {
   return (
-    <div className="flex justify-center h-3/6 ">
-      <div className="self-center h-full w-2/5 bg-slate-50 bg-opacity-80">
+    <div className="flex justify-center h-fit ">
+      <div className="flex flex-col gap-4 self-center h-full w-2/5 bg-slate-50 bg-opacity-80 px-16">
         {props.children}
       </div>
     </div>

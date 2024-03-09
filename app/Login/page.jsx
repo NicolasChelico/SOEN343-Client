f2df73e8c055816ae8b9d40c93bb1b21ddc26278
NicolasChelico <Nicolas.chelico@outlook.com>
2024-03-09 14:34:23 -0500

    Login functionalities with db

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -1,24 +1,42 @@
 "use client";
 import Link from "next/link";
-
+import axios from 'axios'
 import { useState, useEffect } from "react";
 import FormHolder from "../Components/FormHolder";
 
 export default function Login() {
-  const [user, setUser] = useState({
-    username: "",
+  const [credentials, setCredentials] = useState({
+    userName: "",
     password: "",
   });
 
   const handleChange = (e) => {
-    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    setCredentials((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   const handleClick = (e) => {
     e.preventDefault();
   };
 
-  console.log(user);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try{
+      const response = await axios.post('http://localhost:8080/User/AuthenticateUser',credentials);
+      console.log(response.data)
+      alert('Works')
+      
+    }catch(error){
+      console.log('Login failed. ' , error.response.data)
+      alert('Wrong credentials. ');
+      
+    }
+
+  }
+
+  console.log(credentials)
+
+  
   return (
     <FormHolder>
       <h1 className="text-4xl py-8 text-center font-sans">Welcome Back! </h1>
@@ -26,7 +44,7 @@ export default function Login() {
         <label className="text-xl">Username</label>
         <input
           type="text"
-          name="username"
+          name="userName"
           className="ml-4 mr-15 py-3 w-1/2 px-5 rounded-lg border-2 border-black"
           onChange={handleChange}
         />
@@ -41,16 +59,10 @@ export default function Login() {
         />
       </div>
       <div className="flex justify-center gap-4 text-center">
-        <Link
-          className="text-xl rounded-lg py-2 px-6 bg-black text-white  uppercase"
-          href="/SimulatorForm"
-        >
-          Sign In
+        <Link className="text-xl rounded-lg py-2 px-6 bg-black text-white" href="/">
+          <button onClick={handleLogin}className="uppercase"> Sign In</button>
         </Link>
-        <Link
-          className="text-xl rounded-lg py-2 px-6 bg-black text-white  uppercase"
-          href="/SignUp"
-        >
+        <Link className="text-xl rounded-lg py-2 px-6 bg-black text-white  uppercase" href="/SignUp">
           Sign Up
         </Link>
       </div>

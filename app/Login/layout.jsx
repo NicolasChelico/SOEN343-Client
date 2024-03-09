53751a7d6d343bd4b90fff3b63fcf9eb4dc8849c
Houssam Righi <hrighi01@gmail.com>
2024-03-09 12:22:59 -0500

    Refactor login and signup layouts

--- app/Login/layout.jsx
+++ app/Login/layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HomeWrapper from "../Components/HomeWrapper";
 import Navbar from "../Components/Navbar/Navbar";
 
-export default function DashboardLayout({
+export default function LoginLayout({
   children, // will be a page or nested layout
 }) {
   return (
---
+++ app/SignUp/layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeWrapper from "../Components/HomeWrapper";
+import Navbar from "../Components/Navbar/Navbar";
+
+export default function SignUpLayout({
+  children, // will be a page or nested layout
+}) {
+  return (
+    <HomeWrapper>
+      <Navbar></Navbar>
+      {children}
+    </HomeWrapper>
+  );
+}

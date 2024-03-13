29ded180c27b0bd2afc56246a2b7ef5edee0e6cc
Houssam Righi <hrighi01@gmail.com>
2024-03-13 14:14:09 -0400

    Refactor door and light components to use toggleSmartElement function

--- app/SmartElements/light.jsx
+++ app/SmartElements/light.jsx
@@ -1,24 +1,26 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { toggleLight } from "../lib/home";
+import { toggleLight, toggleSmartElement } from "../lib/home";
 
 export default function Light({ lightData, roomId }) {
   const [light, setLight] = useState(lightData);
 
-  const handleCLick = async () => {
-    const updatedLight = await toggleLight(roomId, light.elementId);
+  const handleClick = async () => {
+    console.log(`Toggling light: ${light.elementId} in room: ${roomId}`);
+    const updatedLight = await toggleSmartElement(roomId, light.elementId);
+
     setLight(updatedLight);
-    console.log(`Toggle light: ${light.elementId} in room: ${roomId}`);
+    console.log(`Toggled light: ${light.elementId} in room: ${roomId}`);
   };
 
   useEffect(() => {
     setLight(lightData);
-  }, [lightData]);
+  }, []);
 
   return (
     <button
-      onClick={handleCLick}
-      className={`p-2 m-1 rounded-full ${
+      onClick={handleClick}
+      className={`p-2 m-1 h-4 w-4 rounded-full ${
         light.isOpen ? "bg-yellow-500" : "bg-gray-500"
       }`}
     >

cb7cda3c10d0f34238aaf1fcd48be497a8cd8f8c
Houssam Righi <hrighi01@gmail.com>
2024-03-24 00:25:52 -0400

    Refactor Clock component to use async/await for fetching time and updating speed factor

--- app/Components/SideNav/Clock.jsx
+++ app/Components/SideNav/Clock.jsx
@@ -1,57 +1,60 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { getClock, setTimeSpeed } from "@/app/lib/clock";
+import React, { useState, useEffect, useRef } from "react";
 
 const Clock = (props) => {
   const [time, setTime] = useState(new Date());
-  const [speedFactor, setSpeedFactor] = useState(1); // Default speed factor
-  const [simulationContext, setSimulationContext] = useState(props.simulation)
+  const [speedFactor, setSpeedFactor] = useState(1);
+  const [simulationContext, setSimulationContext] = useState(props.simulation);
 
-
+  // Effect to update simulation context and speed factor when props.simulation changes
   useEffect(() => {
     setSimulationContext(props.simulation);
+    setSpeedFactor(1);
   }, [props.simulation]);
 
+  // Effect to fetch the current time from the server every 2 seconds and update the state
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime((prevTime) => {
-        if(!simulationContext){
-          console.log('im here')
-          return prevTime;
-        }
-        const currentTime = new Date(prevTime.getTime() + speedFactor * 1000); // Increase time based on speed factor
-        return currentTime;
-      });
-    }, 1000); // Update time every second
+    const intervalId = setInterval(async () => {
+      const time = await getClock();
+      setTime(new Date(time));
+    }, 2000);
+
     return () => clearInterval(intervalId);
-   // Clean up the interval
-  }, [simulationContext, speedFactor]); // Re-run effect when speed factor changes
-  
-  
-  const handleSpeedChange = e => {
-    let tempSpeed = parseFloat(e.target.value);
-    if(tempSpeed < 0){
-        tempSpeed = ((-1)/tempSpeed);
+  }, []);
+
+  // Handler to change the speed factor
+  const handleSpeedChange = async (e) => {
+    if (!simulationContext) {
+      return;
     }
-    tempSpeed = tempSpeed.toString().slice(0,4)
-    setSpeedFactor(parseFloat(tempSpeed))
-    console.log(tempSpeed)
-  }
+    const tempSpeed = e.target.value;
+    await setTimeSpeed(tempSpeed);
+    setSpeedFactor(parseFloat(tempSpeed));
+  };
 
   return (
     <div>
-        
-        <p>Speed Multiplier.</p>
-        <input 
-            type="range" 
-            min="-5" 
-            max ="100" 
-            onChange={handleSpeedChange}
-            value={speedFactor}
-        />
-       
-      <h2>Time: { time.toLocaleTimeString() }  </h2>
-      <p>Speed Factor: x{speedFactor}</p>
+      <h2>Time: {time.toLocaleTimeString()} </h2>
+
+      <p className="flex">
+        Speed Factor:{" "}
+        <select
+          onChange={handleSpeedChange}
+          className="flex justify-center items-center m-auto text-black disabled:opacity-50 disabled:bg-gray-200 disabled:cursor-not-allowed"
+          value={speedFactor}
+          disabled={!simulationContext}
+        >
+          <option value="1">1x</option>
+          <option value="2">2x</option>
+          <option value="4">4x</option>
+          <option value="8">8x</option>
+          <option value="16">16x</option>
+          <option value="32">32x</option>
+          <option value="64">64x</option>
+        </select>
+      </p>
     </div>
   );
-}
+};
 
 export default Clock;

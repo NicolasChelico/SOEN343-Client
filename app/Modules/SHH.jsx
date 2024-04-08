76c491cdd8fbdb91c87c131c00fafccacdfce0ae
Houssam Righi <hrighi01@gmail.com>
2024-04-07 23:43:19 -0400

    removed console.log

--- app/Modules/SHH.jsx
+++ app/Modules/SHH.jsx
@@ -49,7 +49,6 @@ export default function SHH() {
 
   const handleZoneAssignment = async (e) => {
     e.preventDefault();
-    console.log(assignedSelectedRoom, assignedZone);
     if (!assignedSelectedRoom || !assignedZone) {
       console.log("Please select a room and a zone");
       return;
